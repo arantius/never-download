e2add99d48ffc649901c704ac6cda6586b5acbef
Anthony Lieuallen <alieuall@google.com>
2019-07-24 09:45:06 -0400

    Also rewrite MIME types.

--- never-download.js
+++ never-download.js
@@ -8,26 +8,40 @@ const TYPE_REGEX = /^(image|text)|\bpdf\b/;
 function headerListener(e) {
   let h = e.responseHeaders;
 
-  let disp = h.find(h => h.name.toLowerCase() == 'content-disposition');
-  if (!disp || !disp.value) {
-    // If there's no disposition, then no forced download, so nothing to do.
-    return h;
-  }
-  if (!disp.value.match(ATTACHMENT_REGEX)) {
-    // No forced download, nothing to do.
-    return h;
-  }
-
   let type = h.find(h => h.name.toLowerCase() == 'content-type');
-  let filename = disp.value.match(FILENAME_REGEX);
+  let disp = h.find(h => h.name.toLowerCase() == 'content-disposition');
+  let filename = disp && disp.value.match(FILENAME_REGEX);
   filename = filename && filename[3];
+  let ext = filename && filename.match(EXTENSION_REGEX)
+      || e.url.match(EXTENSION_REGEX);
+  ext = ext && ext[1];
 
-  //console.log('ND type:', type||null, '\ndisp:', disp, '\nfilename:', filename);
-  if ((type && type.value.match(TYPE_REGEX))
-      || (filename && filename.match(FILENAME_REGEX))
-      || (e && e.url && e.url.match(EXTENSION_REGEX))
-  ) {
-    disp.value = disp.value.replace(ATTACHMENT_REGEX, ' inline')
+  // If the content disposition forces download, rewrite that.
+  if (disp && disp.value.match(ATTACHMENT_REGEX)) {
+    if ((type && type.value.match(TYPE_REGEX))
+        || (filename && filename.match(FILENAME_REGEX))
+        || (ext)
+    ) {
+      disp.value = disp.value.replace(ATTACHMENT_REGEX, ' inline')
+    }
+  }
+
+  // If the MIME type forces download, rewrite that.
+  if (type && type.value == 'application/octet-stream') {
+    let m = e && e.url && e.url.match(EXTENSION_REGEX);
+    switch (ext) {
+    case 'gif':
+    case 'jpg':
+    case 'jpeg':
+    case 'png':
+      type.value = 'image/' + ext;
+    case 'pdf':
+      type.value = 'application/pdf';
+      break;
+    case 'txt':
+      type.value = 'text/plain';
+      break;
+    }
   }
 
   return {responseHeaders: h};
